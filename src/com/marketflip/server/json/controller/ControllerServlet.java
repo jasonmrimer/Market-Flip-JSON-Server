a3e7a9eb09312700c3c2a00d5f5e83b4fb445542
David Walters <dwalters423@gmail.com>
2015-09-13 20:15:40 +0100

    Initial commit- we are live!

---
+++ src/com/marketflip/server/json/controller/ControllerServlet.java
@@ -0,0 +1,46 @@
+package com.marketflip.server.json.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class ControllerServlet extends HttpServlet {
+	
+	/**
+	 * Processes all incoming GET requests.
+	 * @param HttpServletRequest req - The http request.
+	 * @param HttpServletResponse resp - The response from the servlet.
+	 */
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		processRequest(req, resp);
+	}
+	
+	/**
+	 * Processes all incoming POST requests.
+	 * @param HttpServletRequest req - The http request.
+	 * @param HttpServletResponse resp - The response from the servlet.
+	 */
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		processRequest(req, resp);
+	}
+	
+	/**
+	 * Primary function to process all requests. The first filter.
+	 * @param HttpServletRequest req - The http request.
+	 * @param HttpServletResponse resp - The response from the servlet.
+	 */
+	private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		
+		String request;
+		request = req.getParameter("q").toString();
+		
+		PrintWriter JSONwriter = resp.getWriter();
+		resp.setContentType("text/plain");
+		JSONwriter.print("Hello " + request + ", MarketFlip is alive!");
+	}
+}
---
+++ src/com/marketflip/server/json/jsonbuilder/JsonBuilder.java
@@ -0,0 +1,20 @@
+package com.marketflip.server.json.jsonbuilder;
+
+public class JsonBuilder {
+	
+	public JsonBuilder(String request) {
+		//TODO: Parse request to determine what user wants.
+		//TODO: Call DataAccessObject (Not created) and get data from SQL
+		//TODO: Create toString method that creates nice looking JSON.
+	}
+
+	
+	/**
+	 * Returns an entire JSON object representation to be called from the server
+	 * based on the parameters parsed in the constructor.
+	 */
+	public String toString() {
+		
+		return "";
+	}
+}
