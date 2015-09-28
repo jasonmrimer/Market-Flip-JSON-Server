f493ea099fb21b788552a888ec0aa1dc938c7d43
David Walters <dwalters423@gmail.com>
2015-09-29 00:32:09 +0100

    Added information and methods to pricing and product classes.

--- src/com/marketflip/products/Price.java
+++ src/com/marketflip/products/Price.java
@@ -1,69 +1,163 @@
 package com.marketflip.products;
 
-import java.util.Locale;
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /*
  * @author David Walters
  * Price holds date related to the price of the object.
- * Updated - 09/18/2015
- * TODO:
- * 	-custom date string parse.
+ * Updated - 09/28/2015
  */
 
 public class Price {
-	
+
 	private double price;
 	private Date date;
-	
-	public Price () {
-		this.price = 0.00;                      
+	private String company;
+
+	/**
+	 * Default constructor for Price, sets the price at 0.00, the date to the
+	 * current time and the company to null.
+	 */
+	public Price() {
+		this.price = 0.00;
+		this.date = Calendar.getInstance().getTime();
+		this.company = null;
+	}
+
+	/**
+	 * Constructor for Price.
+	 * 
+	 * @param price
+	 *            The price for the product.
+	 * @param date
+	 *            The date and time that the price was found.
+	 * @param company
+	 *            The company where the price was found.
+	 */
+	public Price(double price, Date date, String company) {
+		this.price = price;
+		this.date = date;
+		this.company = company;
+	}
+
+	/**
+	 * Constructor for Price. Company is set to null.
+	 * 
+	 * @param price
+	 *            The price for the product.
+	 * @param date
+	 *            The date and time that the price was found.
+	 */
+	public Price(double price, Date date) {
+		this.price = price;
+		this.date = date;
+		this.company = null;
+	}
+
+	/**
+	 * Constructor for Price. Date is set to the current date and time.
+	 * 
+	 * @param price
+	 *            The price for the product.
+	 * @param company
+	 *            The company where the prices was found.
+	 */
+	public Price(double price, String company) {
+		this.price = price;
+		this.company = company;
 		this.date = Calendar.getInstance().getTime();
 	}
-	
+
 	/**
 	 * Returns the price of the Price object
+	 * 
 	 * @return double The price of the price object.
 	 */
-	public double getPrice () {
+	public double getPrice() {
 		return this.price;
 	}
-	
+
 	/**
 	 * Returns the date and time associated with this Price object.
+	 * 
 	 * @return Date The date associated to this Price object.
 	 */
-	public Date getDate () {
+	public Date getDate() {
 		return this.date;
 	}
 	
+	//TODO: getDate that returns a string here.
+
 	/**
 	 * Set the price of this Price object
-	 * @param price The price to be set to the Price object.
+	 * 
+	 * @param price
+	 *            The price to be set to the Price object.
 	 */
-	public void setPrice (double price) {
+	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	/**
-	 * Set the date object associated with the price.
-	 * @param date The Date object associated with the Price object.
+	 * Set the date object associated with the price from java.util.date
+	 * 
+	 * @param date
+	 *            The java.util.Date object associated with the Price object.
 	 */
-	public void setDate (Date date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 	
+	
 	/**
-	 * TODO - Parse string object to set custom date format without needing the date
-	 * Not supported yet.
-	 * @param dateString
+	 * Set the date associated with the price from java.util..date using 
+	 * a string that is formatted in the following format: EEEEE MMMMM yyyy HH:mm:ss
+	 * example: Monday 01 January 2015 22:43:30
+	 * @param date The string to be parsed to a date.
 	 */
-	public void setDate (String dateString) {
-		System.err.println ("Not supported yet.")
+	public void setDate(String date) {
+		try {
+			String pattern = "EEEEE dd MMMMM yyyy HH:mm:ss";
+			SimpleDateFormat format = new SimpleDateFormat(pattern);
+			this.date = format.parse(date);
+		} 
+		catch (ParseException e) {
+			//TODO: Handle error. 
+			System.out.println ("This error isn't handled YET!");
+			e.printStackTrace();
+		}
 	}
 	
-	
-	
+	/**
+	 * Returns the name of the company where the price was found.
+	 * 
+	 * @return String The name of the company where the price was found.
+	 */
+	public String getCompany() {
+		return company;
+	}
+
+	/**
+	 * Sets the name of the company where the price was found.
+	 * 
+	 * @param company
+	 *            The name of the company where the price was found.
+	 */
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	/**
+	 * @return String The object represented as a string in the format of Price:
+	 *         ... Date: dow mon dd hh:mm:ss zzz yyyy ... Company: ...
+	 */
+	@Override
+	public String toString() {
+		return "Price: " + this.price + " Date: " + this.date.toString() +
+				" Company: " + this.company;
+	}
 
 }
--- src/com/marketflip/products/Product.java
+++ src/com/marketflip/products/Product.java
@@ -1,17 +1,162 @@
 package com.marketflip.products;
 
-import java.util.HashMap;
+import java.net.URL;
+import java.util.ArrayList;
 
 /*
  * @author David Walters
- * Product holds all information related to a product and makes appropriate calls to the database.
- * Updated - 09/18/2015
+ * Product holds all information on price points, descriptors and URL to the product on the company's website.
+ * Updated - 09/28/2015
+ * TODO:
+ * 	-comments
  */
 public class Product {
-	
+
 	private String name;
 	private String description;
-	private HashMap<Date, Price>;
+	private String UPC;
+	private String UNSPSC;
+	private String company;
+	private URL linkToProduct;
+	private ArrayList<Price> prices;
+	private double height;
+	private double width;
+	private double length;
+	private double weight;
 
+	public Product() {
+		this.name = null;
+		this.description = null;
+		this.UPC = null;
+		this.UNSPSC = null;
+		this.linkToProduct = null;
+		this.prices = null;
+		this.height = 0.0f;
+		this.width = 0.0f;
+		this.length = 0.0f;
+		this.weight = 0.0f;
+	}
+
+	public Product(String name, String description, String UPC, String UNSPSC, String company, URL linkToProduct,
+			ArrayList<Price> prices, double height, double width, double length, double weight) {
+		this.name = name;
+		this.description = description;
+		this.UPC = UPC;
+		this.UNSPSC = UNSPSC;
+		this.linkToProduct = linkToProduct;
+		this.prices = prices;
+		this.height = height;
+		this.width = width;
+		this.length = length;
+		this.weight = weight;
+	}
+
+	/**
+	 * Returns if the product can reasonably be considered a match with the current Product.
+	 * @param product The product to compare to this object.
+	 * @return boolean Returns true if the product is a reasonable match. 
+	 */
+	public boolean equals(Product product) {
+		if (this.UPC == product.getUPC()) {
+			return true;
+		} else if (this.UNSPSC == product.getUNSPSC()) {
+			return true;
+		} else if (this.description == product.getDescription()) {
+			return true;
+		} else if (this.name == product.getName()) {
+			if (this.weight * 1.1 >= product.getWeight() && this.weight * 0.9 <= product.getWeight()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUPC() {
+		return UPC;
+	}
+
+	public void setUPC(String uPC) {
+		UPC = uPC;
+	}
+
+	public String getUNSPSC() {
+		return UNSPSC;
+	}
+
+	public void setUNSPSC(String uNSPSC) {
+		UNSPSC = uNSPSC;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public URL getLinkToProduct() {
+		return linkToProduct;
+	}
+
+	public void setLinkToProduct(URL linkToProduct) {
+		this.linkToProduct = linkToProduct;
+	}
+
+	public ArrayList<Price> getPrices() {
+		return prices;
+	}
+
+	public void setPrices(ArrayList<Price> prices) {
+		this.prices = prices;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
 
 }
